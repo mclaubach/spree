7eda9da2fbe9597cfe4fc55655eff258297f5dba
Matthew <mclaubach@gmail.com>
2015-11-30 21:34:58 +0000

    gfdgfd

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,7 +10,6 @@ class EventsController < ApplicationController
   # GET /events/1
   # GET /events/1.json
   def show
-    @event = Event.find(params[:id])
     @bids = @event.bids.all
     @bid = @event.bid.build
   end
