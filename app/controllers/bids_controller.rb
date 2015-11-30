74fbe9bf3926d8da6b87a1b61ce4ad6563049898
Matthew <mclaubach@gmail.com>
2015-11-30 03:25:21 +0000

    hmm

--- app/controllers/bids_controller.rb
+++ app/controllers/bids_controller.rb
@@ -27,6 +27,7 @@ class BidsController < ApplicationController
   def create
     @bid = Bid.new(bid_params)
     @bid.user_id = current_user.id
+    @events = Event.all
     respond_to do |format|
       if @bid.save
         format.html { redirect_to @bid, notice: 'Bid was successfully created.' }
