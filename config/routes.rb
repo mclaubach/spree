343b31bb399a81352376eaaf500d2280966faacb
Matthew <mclaubach@gmail.com>
2015-11-30 17:31:08 +0000

    routesmb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   mount Upmin::Engine => '/admin'
   root to: 'visitors#index'
   devise_for :users
-  resources :users, 
- resources :events do
-  resources :bids
+  resources :users
+  resources :events do
+  	resources :bids
+  end
 end
