1acca34f0e2de236c9e417bd5e68beed58358906
Ryan Williams <ryancwilliams9@gmail.com>
2016-02-27 23:18:09 -0800

    unleash the test beast

---
+++ app/workers/test_world_worker.rb
@@ -0,0 +1,24 @@
+require 'securerandom'
+
+class TestWorldWorker
+  include Sidekiq::Worker
+  include Sidetiq::Schedulable
+
+  recurrence { minutely(10) }
+
+  def perform
+    5.times do
+      User.create!(email: "#{SecureRandom.hex}@example.com", name: 'john', password: 'joejoejoe', password_confirmation: 'joejoejoe')
+    end
+
+    20.times do
+      offset = rand(User.count)
+      rand_record = User.offset(offset).first.id
+      offset = rand(Event.count)
+      rand_event = Event.offset(offset).first
+      offset = rand(2)
+      rand_choice = rand_event.teams[offset].id
+      BidWorker.perform_async(rand_record, rand_event.id, rand_choice)
+    end
+  end
+end
